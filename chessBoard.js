0d0db36ea3d1b76cbe817e84b308e1bb96c54c93
Alon kaminer <giladabargel100@gmail.com>
2022-04-17 13:44:12 +0300

    hiii

--- chessBoard.js
+++ chessBoard.js
@@ -1,19 +1,22 @@
-let divElement = document.createElement('div');
-document.body.appendChild('divElement');
-let container = document.getElementById('container');
-divElement.appendChild('container');
-let box1 = document.getElementById('box1');
-let box2 = document.getElementById('box2');
-for (let y = 0; y < 8; y++) {
-    for (let x = 0; x < 8; x++) {
-        if ((x + y) % 2 === 0) {
-            container.appendChild('box1');
+function myChess() {
+    let divElement = document.createElement('div');
+    document.body.appendChild('divElement');
+    let container = document.getElementById('container');
+    divElement.appendChild('container');
+    let box1 = document.getElementById('box1');
+    let box2 = document.getElementById('box2');
+    for (let y = 0; y < 8; y++) {
+        for (let x = 0; x < 8; x++) {
+            if ((x + y) % 2 === 0) {
+                container.appendChild('box1');
+            }
+            else {
+                container.appendChild('box2');
+            }
         }
-        else {
-            container.appendChild('box2');
-        }
+        let newLine = document.write("\n");
+        container.appendChild('newLine');
     }
-    let newLine = document.write("\n");
-    container.appendChild('newLine');
+    console.log(container);
 };
-console.log(container);
+window.addEventListener('load', myChess);
